e92ecdd5f18936938eb493cec97dace304cad215
swsmontanvs <swsmontanvs@gmail.com>
2026-03-28 05:37:09 +0100

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,27 @@
+// aktuális nyelv lekérése
+function getLanguage() {
+  let lang = localStorage.getItem("lang");
 
+  if (!lang) {
+    // automatikus beállítás
+    if (navigator.language.startsWith("hu")) {
+      lang = "hu";
+    } else {
+      lang = "en";
+    }
+
+    localStorage.setItem("lang", lang);
+  }
+
+  return lang;
+}
+
+// nyelv beállítása
+function setLanguage(lang) {
+  localStorage.setItem("lang", lang);
+  console.log("Language switched to:", lang);
+}
+
+// indításkor
+const currentLang = getLanguage();
+console.log("Current language:", currentLang);
