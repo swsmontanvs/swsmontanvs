6e18650a8f1a6194b67827a4410355e24d180b27
swsmontanvs <swsmontanvs@gmail.com>
2026-04-28 23:35:58 +0200

    Update content-logo.js

--- content-logo.js
+++ content-logo.js
@@ -1,29 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const logo = document.querySelector(".content-logo-img");
 
-  let isActive = false;
-  let cooldownTimer = null;
-
   logo.addEventListener("mouseenter", () => {
-    if (isActive) return; // már aktív → ne indítsuk újra
-
-    isActive = true;
-    clearTimeout(cooldownTimer);
-
-    logo.classList.remove("cooldown");
     logo.classList.add("active");
   });
 
   logo.addEventListener("mouseleave", () => {
-    if (!isActive) return;
-
-    isActive = false;
-
     logo.classList.remove("active");
-    logo.classList.add("cooldown");
-
-    cooldownTimer = setTimeout(() => {
-      logo.classList.remove("cooldown");
-    }, 800);
   });
 });
